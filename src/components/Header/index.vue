7c1e1ad53fc289731aa03ac1ed35e0463969a7ab
Vahy <vahy.the@gmail.com>
2019-01-30 23:59:19 +0100

    more 🌍

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -5,6 +5,89 @@
 <template>
   <header class="Header">
     <img-logo />
+    <svg class="Header__svg" xmlns="http://www.w3.org/2000/svg" width="1497" height="495">
+      <g fill="#fff">
+        <path d="M448.5 172a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM973.5 416a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM313.5 397a6.5 6.5 0 1 1 0 12.999 6.5 6.5 0 0 1 0-13zM1204.5 0a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM6.5 165a6.5 6.5 0 1 1 0 13 6.5 6.5 0 0 1 0-13zM1488.5 198a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM1273.5 253a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM1152 181a7 7 0 1 1 0 14 7 7 0 0 1 0-14zM505.5 65a5.5 5.5 0 1 1 0 11 5.5 5.5 0 0 1 0-11zM481.5 478a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM258.5 331a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM600 313a7 7 0 1 1 0 14 7 7 0 0 1 0-14zM270.5 207a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9zM830.5 161a6.5 6.5 0 1 1 0 13 6.5 6.5 0 0 1 0-13zM299.5 150a6.5 6.5 0 1 1 0 13 6.5 6.5 0 0 1 0-13zM775 49a4 4 0 1 1 0 8 4 4 0 0 1 0-8zM779 129a4 4 0 1 1 0 8 4 4 0 0 1 0-8zM891 203a4 4 0 1 1 0 8 4 4 0 0 1 0-8zM652.5 100a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17zM1090 74a4 4 0 1 1 0 8 4 4 0 0 1 0-8zM315 225a5 5 0 1 1 0 10 5 5 0 0 1 0-10zM154 149a5 5 0 1 1 0 10 5 5 0 0 1 0-10zM174.5 245a8.5 8.5 0 1 1 0 17 8.5 8.5 0 0 1 0-17z" opacity=".08"/>
+      </g>
+      <g class="planet-with-rings">
+       <svg xmlns="http://www.w3.org/2000/svg" width="383" height="66">
+         <linearGradient id="a" x1="401.321" x2="442.442" y1="39.168" y2="90.981" gradientTransform="rotate(-7.82 143.769 1648.121)" gradientUnits="userSpaceOnUse">
+           <stop offset=".088" stop-color="#ed1e79"/>
+           <stop offset=".686" stop-color="#ed1e79"/>
+         </linearGradient>
+         <circle cx="194.382" cy="32.641" r="32.348" fill="url(#a)"/>
+         <linearGradient id="b" x1="401.311" x2="442.432" y1="39.167" y2="90.98" gradientTransform="rotate(-7.82 143.769 1648.121)" gradientUnits="userSpaceOnUse">
+           <stop offset=".088" stop-color="#ed1e79"/>
+           <stop offset=".686" stop-color="#ed1e79"/>
+         </linearGradient>
+         <circle cx="194.373" cy="32.641" r="32.348" fill="url(#b)"/>
+         <linearGradient id="c" x1="440.559" x2="384.713" y1="25.171" y2="55.042" gradientTransform="rotate(-7.82 143.769 1648.121)" gradientUnits="userSpaceOnUse">
+           <stop offset="0" stop-color="#ffeca6" stop-opacity="0"/>
+           <stop offset="1" stop-color="#ffeca6" stop-opacity=".4"/>
+         </linearGradient>
+         <path fill="url(#c)" d="M215.494 8.134c-6.783-5.838-15.943-8.853-25.508-7.541-17.699 2.428-30.08 18.744-27.652 36.443.326 2.378.906 4.66 1.703 6.823 8.492 2.878 20.031 1.563 30.777-4.325 14.027-7.685 22.216-20.703 20.68-31.4z"/>
+         <linearGradient id="d" x1="427.031" x2="389.939" y1="21.946" y2="41.787" gradientTransform="rotate(-7.82 143.769 1648.121)" gradientUnits="userSpaceOnUse">
+           <stop offset="0" stop-color="#ffeca6" stop-opacity="0"/>
+           <stop offset="1" stop-color="#ffeca6" stop-opacity=".4"/>
+         </linearGradient>
+         <path fill="url(#d)" d="M163.849 21.941c4.066 5.184 14.323 5.348 23.83.139C197.796 16.536 203.24 7 199.87.754a32.304 32.304 0 0 0-9.883-.161c-12.436 1.706-22.246 10.267-26.138 21.348z"/>
+         <linearGradient id="e" x1="447.082" x2="407.527" y1="63.173" y2="84.33" gradientTransform="rotate(-7.82 143.769 1648.121)" gradientUnits="userSpaceOnUse">
+           <stop offset="0" stop-color="#1b1464" stop-opacity=".1"/>
+           <stop offset="1" stop-color="#1b1464" stop-opacity=".4"/>
+         </linearGradient>
+         <path fill="url(#e)" d="M225.871 40.077c-1.252-1.311-3.072-1.55-5.585 1.36-5.792 6.707-8.402 5.658-20.783 7.856-9.33 1.657-14.125 9.42-10.71 15.216a32.33 32.33 0 0 0 9.985.179c13.632-1.869 24.107-11.977 27.093-24.611z"/>
+         <linearGradient id="f" x1="1255.215" x2="1305.24" y1="-13.681" y2="-55.657" gradientTransform="rotate(-90 770.966 541.913)" gradientUnits="userSpaceOnUse">
+           <stop offset=".107" stop-color="#190628"/>
+           <stop offset="1" stop-color="#474747" stop-opacity="0"/>
+         </linearGradient>
+         <circle cx="194.382" cy="32.651" r="32.651" fill="url(#f)" opacity=".7"/>
+         <circle cx="194.382" cy="32.651" r="32.651" fill="#231F20" opacity=".1"/>
+         <path fill="#FFF" d="M224.316 20.36c.161.394.313.792.459 1.194 18.764-1.244 148.37-17.316 148.75-14.534.571 4.161-143.682 28.155-178.147 32.882C160.914 44.63 9.909 60.021 9.338 55.861c-.381-2.783 134.384-21.86 152.79-25.714.033-.426.071-.851.121-1.273C138.33 33.656-.497 54.01.001 57.642c.692 5.046 156.006-10.206 195.665-15.645 39.659-5.44 188.024-30.729 187.332-35.775-.499-3.632-134.359 12.304-158.682 14.138z" opacity=".14"/>
+         <path fill="#FFF" d="M225.255 22.978c.072.232.142.464.209.698 11.558-.602 135.845-16.916 136.072-15.261.399 2.91-138.641 25.155-166.501 28.976C167.172 41.212 21.66 56.884 21.261 53.975c-.227-1.654 129.48-19.066 140.774-21.599.001-.243.006-.486.014-.728C149.54 34.542 19.001 52.431 19.285 54.497c.48 3.498 146.958-11.821 175.929-15.795 28.972-3.974 168.846-26.82 168.366-30.317-.284-2.065-125.499 14.012-138.325 14.593z" opacity=".14"/>
+       </svg>
+     </g>
+      <g class="planet-grey">
+        <svg xmlns="http://www.w3.org/2000/svg" width="137" height="137">
+          <circle cx="68.5" cy="68.5" r="68.5" fill="#9E9E9E"/>
+          <linearGradient id="aneco" x1="121.409" x2="57.489" y1="7.044" y2="41.234" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#eaeaea" stop-opacity="0"/>
+            <stop offset="1" stop-color="#eaeaea" stop-opacity=".4"/>
+          </linearGradient>
+          <path fill="url(#aneco)" d="M132.114 43.054C122.012 17.823 97.339 0 68.5 0a68.32 68.32 0 0 0-24.83 4.642 21.516 21.516 0 0 0-2.937 6.896C36.766 28.784 55.08 47.72 81.639 53.828c21.19 4.874 41.226.111 50.475-10.774z"/>
+          <linearGradient id="bneco" x1="136.691" x2="46.704" y1="29.443" y2="77.576" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#eaeaea" stop-opacity="0"/>
+            <stop offset="1" stop-color="#eaeaea" stop-opacity=".4"/>
+          </linearGradient>
+          <path fill="url(#bneco)" d="M120.3 113.324C130.704 101.31 137 85.64 137 68.5 137 30.668 106.332 0 68.5 0 51.532 0 36.007 6.173 24.042 16.391c-7.695 26.001 4.309 58.367 31.721 79.363 20.165 15.447 44.096 21.238 64.537 17.57z"/>
+          <linearGradient id="cneco" x1="9.343" x2="77.197" y1="117.71" y2="89.846" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#474747" stop-opacity="0"/>
+            <stop offset="1" stop-color="#474747" stop-opacity=".4"/>
+          </linearGradient>
+          <path fill="url(#cneco)" d="M68.5 137c5.259 0 10.38-.596 15.3-1.718-.194-3.631-2.031-7.867-6.271-12.907-9.791-11.699-23.121-8.428-32.625-19.229-7.729-8.403-14.563-18.741-21.702-27.662-7.653-9.746-17.455-7.273-22.446 1.571-.064.112-.119.232-.18.347C4.939 111.027 33.686 137 68.5 137z"/>
+          <linearGradient id="dneco" x1="686.995" x2="699.236" y1="9.121" y2="-1.943" gradientTransform="rotate(-20.149 710.345 1745.06)" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#474747" stop-opacity="0"/>
+            <stop offset="1" stop-color="#474747" stop-opacity=".4"/>
+          </linearGradient>
+          <path fill="url(#dneco)" d="M108.386 107.386c2.06 5.613-1.482 12.077-7.909 14.435-6.428 2.358-13.309-.281-15.369-5.895-2.06-5.615 1.481-12.078 7.91-14.436 6.427-2.359 13.308.28 15.368 5.896z"/>
+          <linearGradient id="eneco" x1="-984.594" x2="-975.043" y1="1671.019" y2="1663.059" gradientTransform="matrix(.4077 .9925 -1.2337 .2117 2514.09 721.3)" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#474747" stop-opacity="0"/>
+            <stop offset="1" stop-color="#474747" stop-opacity=".4"/>
+          </linearGradient>
+          <path fill="url(#eneco)" d="M58.728 107.583c-2.924-.609-4.844-3.274-4.287-5.952.559-2.678 3.383-4.356 6.307-3.746 2.925.61 4.844 3.275 4.286 5.953-.558 2.678-3.381 4.354-6.306 3.745z"/>
+          <linearGradient id="fneco" x1="41.06" x2="69.307" y1="13.222" y2="8.279" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#eaeaea" stop-opacity="0"/>
+            <stop offset="1" stop-color="#eaeaea" stop-opacity=".4"/>
+          </linearGradient>
+          <path fill="url(#fneco)" d="M59.394 16.316c-5.663.808-10.648-1.289-11.133-4.684-.485-3.396 3.713-6.805 9.375-7.612 5.664-.81 10.648 1.288 11.133 4.684.483 3.395-3.714 6.803-9.375 7.612z"/>
+          <linearGradient id="g" x1="16.025" x2="120.976" y1="112.532" y2="24.468" gradientUnits="userSpaceOnUse">
+            <stop offset=".107" stop-color="#190628"/>
+            <stop offset="1" stop-color="#474747" stop-opacity="0"/>
+          </linearGradient>
+          <circle cx="68.5" cy="68.5" r="68.5" fill="url(#g)" opacity=".7"/>
+          <circle cx="68.5" cy="68.5" r="68.5" fill="#231F20" opacity=".1"/>
+        </svg>
+      </g>
+    </svg>
   </header>
 </template>
 
